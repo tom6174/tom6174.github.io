7c5751f721d20fb849e20a44dca9eb397311febd
tom6174 <thomas78.song@gmail.com>
2022-08-15 20:31:15 +0900

    1st commit

--- tmp_data/population_callbacks.py
+++ tmp_data/population_callbacks.py
@@ -5,109 +5,79 @@ from dash.dependencies import Input, Output, ALL, State, MATCH, ALLSMALLER
 import plotly.express as px
 import pandas as pd
 import numpy as np
+from numpy import int64
 
-df = pd.read_csv("https://raw.githubusercontent.com/tom6174/tom6174.github.io/main/tmp_data/2021-population.csv", engine='python', encoding='utf-8')
-df.rename(columns={'광역': '광역', '군구': '군구'}, inplace=True)
+df = pd.read_csv("https://raw.githubusercontent.com/tom6174/tom6174.github.io/main/tmp_data/2021-population.csv", engine='python', encoding='utf-8', dtype={ "총인구": int64 })
+df.rename(columns={'광역': '광역시도', '군구': '시군구'}, inplace=True)
 
-print(df)
+print(df['광역시도'].unique())
 
-# app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# app.layout = html.Div([
-#     html.Div(children=[
-#         html.Button('Add Chart', id='add-chart', n_clicks=0),
-#     ]),
-#     html.Div(id='container', children=[])
-# ])
+app.layout = html.Div([
+    html.H1("대한민국 인구 분포", style={"textAlign":"center"}),
+    html.Hr(),
+    html.P("시군구별 인구:"),
+    html.Div(html.Div([
+        dcc.Dropdown(id='광역시도', clearable=False,
+                     value="서울특별시",
+                     options=[{'label': x, 'value': x} for x in
+                              df["광역시도"].unique()]),
+    ],className="two columns"),className="row"),
 
+    html.Div(id="output-div", children=[]),
+])
 
-# @app.callback(
-#     Output('container', 'children'),
-#     [Input('add-chart', 'n_clicks')],
-#     [State('container', 'children')]
-# )
-# def display_graphs(n_clicks, div_children):
-#     new_child = html.Div(
-#         style={'width': '45%', 'display': 'inline-block', 'outline': 'thin lightgrey solid', 'padding': 10},
-#         children=[
-#             dcc.Graph(
-#                 id={
-#                     'type': 'dynamic-graph',
-#                     'index': n_clicks
-#                 },
-#                 figure={}
-#             ),
-#             dcc.RadioItems(
-#                 id={
-#                     'type': 'dynamic-choice',
-#                     'index': n_clicks
-#                 },
-#                 options=[{'label': 'Bar Chart', 'value': 'bar'},
-#                          {'label': 'Line Chart', 'value': 'line'},
-#                          {'label': 'Pie Chart', 'value': 'pie'}],
-#                 value='bar',
-#             ),
-#             dcc.Dropdown(
-#                 id={
-#                     'type': 'dynamic-dpn-s',
-#                     'index': n_clicks
-#                 },
-#                 options=[{'label': s, 'value': s} for s in np.sort(df['광역'].unique())],
-#                 multi=True,
-#                 value=["서울특별시", "부산광역시", "대구광역시", "대전광역시", "광주광역시", "울산광역시", "세종특별자치시", "경기도", "강원도", "충청남도", "충청북도", "전라남도", "전라북도", "경상남도", "경상북도", "제주도"],
-#             ),
-#             dcc.Dropdown(
-#                 id={
-#                     'type': 'dynamic-dpn-ctg',
-#                     'index': n_clicks
-#                 },
-#                 options=[{'label': c, 'value': c} for c in ['군구', '내국인계', '외국인계']],
-#                 value='군구',
-#                 clearable=False
-#             ),
-#             dcc.Dropdown(
-#                 id={
-#                     'type': 'dynamic-dpn-num',
-#                     'index': n_clicks
-#                 },
-#                 options=[{'label': n, 'value': n} for n in ['총인구', '내국인(남)', '내국인(여)', '외국인계']],
-#                 value='외국인계',
-#                 clearable=False
-#             )
-#         ]
-#     )
-#     div_children.append(new_child)
-#     return div_children
+@app.callback(Output(component_id="output-div", component_property="children"),
+              Input(component_id="광역시도", component_property="value"),
+)
+def make_graphs(province_chosen):
+    # HISTOGRAM
+    df_bar = df[(df["광역시도"]==province_chosen) & ~(df["시군구"]==province_chosen)]
+    print(df_bar)
+    print(df_bar.dtypes)
+    fig_hist = px.line(df_bar, x="시군구", y="총인구")
+    # fig_hist.update_xaxes(categoryorder="total descending")
 
+    # # STRIP CHART
+    # fig_strip = px.strip(df_hist, x="animal_stay_days", y="intake_type")
 
-# # @app.callback(
-# #     Output({'type': 'dynamic-graph', 'index': MATCH}, 'figure'),
-# #     [Input(component_id={'type': 'dynamic-dpn-s', 'index': MATCH}, component_property='value'),
-# #      Input(component_id={'type': 'dynamic-dpn-ctg', 'index': MATCH}, component_property='value'),
-# #      Input(component_id={'type': 'dynamic-dpn-num', 'index': MATCH}, component_property='value'),
-# #      Input({'type': 'dynamic-choice', 'index': MATCH}, 'value')]
-# # )
-# # def update_graph(s_value, ctg_value, num_value, chart_choice):
-# #     print(s_value)
-# #     dff = df[df['광역'].isin(s_value)]
+    # # SUNBURST
+    # df_sburst = df.dropna(subset=['chip_status'])
+    # df_sburst = df_sburst[df_sburst["intake_type"].isin(["STRAY", "FOSTER", "OWNER SURRENDER"])]
+    # fig_sunburst = px.sunburst(df_sburst, path=["animal_type", "intake_type", "chip_status"])
 
-# #     if chart_choice == 'bar':
-# #         dff = dff.groupby([ctg_value], as_index=False)[['총인구', '내국인(남)', '내국인(여)', '외국인계']].sum()
-# #         fig = px.bar(dff, x=ctg_value, y=num_value)
-# #         return fig
-# #     elif chart_choice == 'line':
-# #         if len(s_value) == 0:
-# #             return {}
-# #         else:
-# #             dff = dff.groupby([ctg_value, '군구'], as_index=False)[['총인구', '내국인(남))', '내국인(여)', '외국인계']].sum()
-# #             fig = px.line(dff, x='군구', y=num_value, color=ctg_value)
-# #             return fig
-# #     elif chart_choice == 'pie':
-# #         fig = px.pie(dff, names=ctg_value, values=num_value)
-# #         return fig
+    # # Empirical Cumulative Distribution
+    # df_ecdf = df[df["animal_type"].isin(["DOG","CAT"])]
+    # fig_ecdf = px.ecdf(df_ecdf, x="animal_stay_days", color="animal_type")
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+    # # LINE CHART
+    # df_line = df.sort_values(by=["intake_time"], ascending=True)
+    # df_line = df_line.groupby(
+    #     ["intake_time", "animal_type"]).size().reset_index(name="count")
+    # print(df_line.head())
+    # fig_line = px.line(df_line, x="intake_time", y="count",
+    #                    color="animal_type", markers=True)
+
+    return [
+        html.Div([
+            html.Div([dcc.Graph(figure=fig_hist)], className="twelve columns"),
+            # html.Div([dcc.Graph(figure=fig_strip)], className="six columns"),
+        ], className="row"),
+        # html.H2("All Animals", style={"textAlign":"center"}),
+        # html.Hr(),
+        # html.Div([
+        #     html.Div([dcc.Graph(figure=fig_sunburst)], className="six columns"),
+        #     html.Div([dcc.Graph(figure=fig_ecdf)], className="six columns"),
+        # ], className="row"),
+        # html.Div([
+        #     html.Div([dcc.Graph(figure=fig_line)], className="twelve columns"),
+        # ], className="row"),
+    ]
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
     
     
